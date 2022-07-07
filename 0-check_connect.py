156046d8d20ae2ed8b3895c36e99bfacf8e7136a
sadegh-dev <sadegh.rez@gmail.com>
2022-07-07 16:55:45 +0430

    #check database -insert try-except

--- 0-check_connect.py
+++ 0-check_connect.py
@@ -1,8 +1,12 @@
 import mysql.connector
 
 
-mydb = mysql.connector.connect (
-    host ='localhost' ,
-    user = 'debian-sys-maint' ,
-    password = 'xkfnnlgUOSuVdAjg'
-)
+try :
+    mydb = mysql.connector.connect (
+        host ='localhost' ,
+        user = 'debian-sys-maint' ,
+        password = 'xkfnnlgUOSuVdAjg'
+    )
+    print('Connected to MySQL DataBase...')
+except :
+    print('Error Connection !')
